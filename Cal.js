2a35e8da840acc74eaeb0b0387db961e5c6551a9
HamzaAlwan <ihamzaalwan@gmail.com>
2018-02-28 15:54:50 +0200

    Second

--- Cal.js
+++ Cal.js
@@ -1,61 +1,10 @@
 $( document ).ready(function() {
-function calculator (){
-  var result = {}
 
-  result.sum = sum;
-  result.subtraction = subtraction;
-  result.multi = multi;
-  result.divide = divide;
-  result.module = module;
-  result.square = square;
-  result.squareRoot = squareRoot;
-  result.negate = negate;
 
-  return result
-}
 
-this.sum = function (a, b){
-	return a + b;
-}
 
-this.subtraction = function (a, b){
-	return a - b;
-}
 
-this.multi = function (a, b){
-	return a * b;
-}
 
-this.divide = function (a, b){
-	return a / b;
-}
-
-this.module = function (a, b){
-	return a % b;
-}
-
-this.square = function (a){
-	return a ** 2;
-}
-
-this.squareRoot = function (a){
-	return Math.sqrt(a);
-}
-
-this.negate = function (a){
-	if (a > 0){
-		return -a;
-	}if (a < 0){
-		return -a;
-	}
-	return 0;
-}
-
-this.clearAll = function (a){
-	return 0
-}
-
-var a = $("#a").val()
 });
 
 
